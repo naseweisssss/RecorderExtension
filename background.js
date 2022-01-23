668986caeb56cf596990e6d28654dbcde55bcc35
JiaGengChang <jiageng1997@gmail.com>
2022-01-23 10:27:30 +0000

    second commit

--- background.js
+++ background.js
@@ -1,7 +1,28 @@
-let color = '#3aa757';
+// let color = '#3aa757';
 
+function getActiveTab() {
+  return chrome.tabs.query({active: true, currentWindow: true});
+}
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
-});
+//when extension is reloaded or installed
+// chrome.runtime.onInstalled.addListener(() => {
+//   chrome.storage.sync.set({ color });
+//   console.log('Default background color set to %cgreen', `color: ${color}`);
+// });
+
+//when demonstrator navigates from one url to another
+// chrome.tabs.onUpdated.addListener(getActiveTab().then((tabs) => {
+//   console.log("navigated to new page");
+//   chrome.tabs.sendMessage(tabs[0].id, {action: "record_next_step"});
+// }));
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+  if (changeInfo.status == 'complete'){
+    console.log("navigated to new page");
+    // getActiveTab().then((tabs)=>{
+    //   console.log(tabs);
+    //   chrome.tabs.sendMessage(tabs[0].id, {action: "record_next_step"});
+    // });
+    chrome.tabs.sendMessage(tabId, {action: "record_next_step"});
+  }
+})
--- popup.js
+++ popup.js
@@ -1,57 +1,152 @@
 // Initialize buttons
-let changeColor = document.getElementById("changeColor");
+// let changeColor = document.getElementById("changeColor");
 let startRecordingBtn = document.getElementById("startRecordingBtn");
 let endRecordingBtn = document.getElementById("endRecordingBtn");
 let saveRecordingBtn = document.getElementById("saveRecordingBtn");
 
 // initial state
 chrome.storage.local.set({isRecording: false}, function(){});
-chrome.storage.local.set({btnArray : [4,5,6]}, function(){})
+chrome.storage.local.set({btnArray : []}, function(){});
+chrome.storage.local.set({currentURL: ""}, function(){});
 
 // ============================================
-chrome.storage.sync.get("color", ({ color }) => {
-	changeColor.style.backgroundColor = color;
-});
+// styling
+// chrome.storage.sync.get("color", ({ color }) => {
+// 	changeColor.style.backgroundColor = color;
+// });
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-	chrome.scripting.executeScript({
-		target: { tabId: tab.id },
-		function: setPageBackgroundColor,
-	});
-});
+// // When the button is clicked, inject setPageBackgroundColor into current page
+// changeColor.addEventListener("click", async () => {
+// 	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+// 	chrome.scripting.executeScript({
+// 		target: { tabId: tab.id },
+// 		function: setPageBackgroundColor,
+// 	});
+// });
   
-// The body of this function will be executed as a content script inside the
-// current page
-function setPageBackgroundColor() {
-	chrome.storage.sync.get("color", ({ color }) => {
-		document.body.style.backgroundColor = color;
-	});
-}
+// // The body of this function will be executed as a content script inside the
+// // current page
+// function setPageBackgroundColor() {
+// 	chrome.storage.sync.get("color", ({ color }) => {
+// 		document.body.style.backgroundColor = color;
+// 	});
+// }
+// END IGNORE
 // ============================================
 
-
+//when demonstrator presses start recording button
 startRecordingBtn.addEventListener("click", async() => {
-	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-	chrome.scripting.executeScript({
-		target: { tabId: tab.id },
-		function: startRecording,
-	});
+	chrome.storage.local.get(['isRecording'], function(result){
+		if(!result.isRecording){
+			console.log("not recording yet.")
+			//start recording
+			let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+			//reset button array
+			chrome.storage.local.set({btnArray : []}, function(){});
+			chrome.scripting.executeScript({
+				target: { tabId: tab.id },
+				function: recordOneStep,
+				args: [tab.url],
+			});
+		} else {
+			console.log("already recording")
+		}
+	})
+});
+
+//when demonstrator navigates to a different page
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+	// if (request.action == 'record_next_step') {
+	// 	alert("Record next step!");
+	// }
+	console.log("Message from background script:")
+	console.log(request.action);
 })
 
-function startRecording () {
-	console.log("starting recording")
+//when demonstrator presses end recording button
+endRecordingBtn.addEventListener("click", async() => {
+	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+	console.log("ending recording");
+	chrome.scripting.executeScript({
+		target: { tabId: tab.id },
+		function: endRecording,
+	});
+});
+
+//when demonstrator presses save recording button
+saveRecordingBtn.addEventListener("click", async() => {
+	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+	console.log("saving recording");
+	chrome.scripting.executeScript({
+		target: { tabId: tab.id },
+		function: saveRecording,
+	});
+});
+
+
+// function called on initial page and subsequent pages
+function recordOneStep(taburl) {
+	
+	console.log("starting recording"); //being logged to tab box
+	console.log("taburl: " + taburl); //being logged to tab box
+
+	chrome.storage.local.set({currentURL: taburl});
+
 	document.body.addEventListener("click", function (evt) {
-		// console.dir(this);
-		// const buttonArray = [123];
-		//note evt.target can be a nested element, not the body element, resulting in misfires
-		// console.log(evt.target);
-		chrome.storage.local.get(['btnArray'], function (result) {
-			result.btnArray.push(evt.target);
-			// alert("body clicked");
-			console.log(result.btnArray);
+		let btnSpecifier = evt.target.innerHTML;
+
+		chrome.storage.local.get(['btnArray','currentURL'], function (result) {
+			result.btnArray.push({url: result.currentURL, name: btnSpecifier});
 			chrome.storage.local.set({btnArray: result.btnArray}, function(){});
+			console.log(result.btnArray); //logged to tab box
 		});
 	});
+}
+
+function download(filename, text) {
+    var pom = document.createElement('a');
+    pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    pom.setAttribute('download', filename);
+
+    if (document.createEvent) {
+        var event = document.createEvent('MouseEvents');
+        event.initEvent('click', true, true);
+        pom.dispatchEvent(event);
+    }
+    else {
+        pom.click();
+    }
+}
+
+function endRecording(){
+
+	chrome.storage.local.get(['isRecording'], function (result){
+		if (result.isRecording){
+			chrome.storage.local.set({isRecording: false}, function(){updateRecordingStatus(result.isRecording)})
+		} else {
+			console.log("no recording was started.")
+		}
+	})
+}
+
+function saveRecording(){
+
+	//write btnArray to a file - local file for now 
+	console.log("ending recording. saving navigation history");
+
+	chrome.storage.local.get(['btnArray'], function(result){
+		download('outfile.txt', JSON.stringify(result.btnArray));
+	})
+
+}
+
+function updateRecordingStatus(stopRecording){
+	//change the innerHTML of the div id=recordingStatus 
+	let statusDiv = document.getElementById("recordingStatus");
+	if (stopRecording) {
+		statusDiv.style.textContent = 'Recording...';
+	} else {
+		statusDiv.style.textContent = '';
+	}
+	
 }
